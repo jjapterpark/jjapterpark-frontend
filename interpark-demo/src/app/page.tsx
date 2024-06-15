ee878d8b22c11bd850c7ecc91a81e768cf8c4e7c
Haru-arp <vr1997@naver.com>
2024-06-15 23:11:22 +0900

    add

--- interpark-demo/src/app/page.tsx
+++ interpark-demo/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { getImage } from "@/utils/asset";
 import tw from "tailwind-styled-components";
 
 const Contents = tw.div`
@@ -66,7 +67,8 @@ const TagList = tw.div`
 `;
 
 const TagItem = tw.div`
-    first: ml-0
+    first:ml-0
+    ml-[6px]
     inline-block
 `;
 
@@ -85,8 +87,32 @@ const TagButton = tw.div`
     border-[#ddd]
     rounded-[4px]
     box-border
+    bg-[right_8px_top_50%]
+    font-bold
+     after:bg-[url('https://openimage.interpark.com/ticket-desktop/pages/product/icon_tagarrow_new2.png')]
+    after:bg-no-repeat
+    after:relative
+    after:left-[2px]
+    after:inline-block
+    after:w-[9px]
+    after:h-[9px]
+    after:content-['']
+    after:bg-contain
+`;
 
-    
+const IsAccentTagBtn = tw(TagButton)`
+    font-bold
+    border-[#ffe4d5]
+    text-[#ff7262]
+    after:bg-[url('http://openimage.interpark.com/ticket-desktop/pages/product/icon_tagarrow_new.png')]
+    after:bg-no-repeat
+    after:relative
+    after:left-[2px]
+    after:inline-block
+    after:w-[9px]
+    after:h-[9px]
+    after:content-['']
+    after:bg-contain
 `;
 
 const PrdSection = tw.div`
@@ -103,6 +129,87 @@ const TagText = tw.div`
     align-middle
 `;
 
+const SummaryBody = tw.div`
+
+`;
+
+const PosterBox = tw.div`
+    inline-block
+    w-[300px]
+`;
+
+const PosterBoxTop = tw.div`
+    bg-[url('https://openimage.interpark.com/ticket-desktop/pages/product/noImage_logo.png')]
+    bg-no-repeat
+    bg-[#f0f0f0]
+    bg-[50% 50%]
+    relative
+    h-[400px]
+
+`;
+
+const PosterImg = tw.img`
+    block
+    w-full
+    h-full
+    border-[1px]
+    border-solid
+    border-[#dadee3]
+    box-border
+`;
+
+const PosterBoxBottom = tw.div`
+    mt-[12px]
+`;
+
+const PrdCast = tw.div`
+    float-left
+    mt-[2px]
+    
+`;
+
+const Share = tw.div`
+    float-right
+`;
+
+const PrdCastWrap = tw.div`
+    inline-block
+`;
+
+const PrdCastNum = tw.p`
+    inline-block
+    ml-[5px]
+    text-[14px]
+    font-bold
+`;
+
+const PrdCastBtn = tw.a`
+    relative
+    inline-block
+    text-[14px]
+    font-inherit
+    leading-[24px]
+    outline-none
+    cursor-pointer
+    before:bg-[url('https://openimage.interpark.com/ticket-desktop/pages/product/icon_heart.png')]
+    before:bg-no-repeat
+    before:relative
+    before:inline-block
+    before:top-[2px]
+    before:w-[20px]
+    before:h-[15px]
+    before:pr-[6px]
+    before:content-[' ']
+    before:bg-[top_bottom_left_right_0px]
+`;
+
+const ShareList = tw.ul`
+    
+`;
+
+const ShareItem = tw.li`
+`;
+
 export default function Home() {
     return (
         <Contents>
@@ -114,9 +221,11 @@ export default function Home() {
                                 <Tag>
                                     <TagList>
                                         <TagItem>
-                                            <TagButton>asdf</TagButton>
+                                            <IsAccentTagBtn>단독판매</IsAccentTagBtn>
                                         </TagItem>
-                                        <TagItem></TagItem>
+                                        <TagItem>
+                                            <TagButton>안심예매</TagButton>
+                                        </TagItem>
                                     </TagList>
                                 </Tag>
                                 <PrdTitle>싸이흠뻑쇼 SUMMERSWAG2024 - 과천</PrdTitle>
@@ -127,7 +236,26 @@ export default function Home() {
                                     </TagText>
                                 </PrdSection>
                             </SummaryTop>
-                            <div>Body</div>
+                            <SummaryBody>
+                                <PosterBox>
+                                    <PosterBoxTop>
+                                        <PosterImg src="https://ticketimage.interpark.com/Play/image/large/24/24007166_p.gif" />
+                                    </PosterBoxTop>
+                                    <PosterBoxBottom>
+                                        <PrdCast>
+                                            <PrdCastWrap>
+                                                <PrdCastBtn>티켓캐스트</PrdCastBtn>
+                                            </PrdCastWrap>
+                                            <PrdCastNum>3134</PrdCastNum>
+                                        </PrdCast>
+                                        <Share>
+                                            <ShareList>
+                                                <ShareItem></ShareItem>
+                                            </ShareList>
+                                        </Share>
+                                    </PosterBoxBottom>
+                                </PosterBox>
+                            </SummaryBody>
                         </Summary>
                     </ProductMainTop>
                 </ProductMain>
