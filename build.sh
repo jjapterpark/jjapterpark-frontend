1a7b03de7fc6112c5aaa1cf3ba2d20512bf192c3
dev-chpark <xpmxf5@gmail.com>
2024-06-16 14:38:03 +0900

    ⚙️  chore: Test

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 npm install
 npm run build
-mkdir -p ../output
-cp -R .next ../output/.next
-cp -R public ../output/public
-cp package.json ../output/package.json
-cp next.config.mjs ../output/next.config.mjs
+mkdir -p ./output
+cp -R .next ./output/.next
+cp -R public ./output/public
+cp package.json ./output/package.json
+cp next.config.mjs ./output/next.config.mjs
+
