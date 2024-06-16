623c41c23602e2cab3674c30aa7f0556bb714c6c
dev-chpark <xpmxf5@gmail.com>
2024-06-16 14:33:56 +0900

    🚀 CI/CD: build.sh 스크립트 수정: .next 디렉토리를 올바르게 복사하도록 변경

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 npm install
 npm run build
 mkdir -p ../output
-cp -R ./.next ../output
-cp -R ./public ../output
-cp ./package.json ../output
-cp ./next.config.mjs ../output
-
+cp -R .next ../output/.next
+cp -R public ../output/public
+cp package.json ../output/package.json
+cp next.config.mjs ../output/next.config.mjs
